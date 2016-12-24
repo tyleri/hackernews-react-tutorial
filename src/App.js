c805162ab670bc2f69882b468b5e88bb1dc805d7
Tyler Ishikawa <tyi3@cornell.edu>
2016-12-24 06:00:17 -0500

    incorporate stylings into App

--- src/App.js
+++ src/App.js
@@ -32,16 +32,19 @@ class App extends Component {
       list,
       query: ''
     };
-    this.onSearchChange = event => this.setState({ query: event.target.value });
   }
+
+  onSearchChange = event => this.setState({ query: event.target.value });
 
   render() {
     const { query, list } = this.state;
     return (
-      <div className="App">
-        <Search value={query} onChange={this.onSearchChange}>
-          Search
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search value={query} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
         <Table list={list} pattern={query} />
       </div>
     );
@@ -54,14 +57,14 @@ const Search = ({ value, onChange, children }) =>
   </form>
 
 const Table = ({ list, pattern }) =>
-  <div>
+  <div className="table">
     { list.filter(isSearched(pattern)).map( item =>
-      <div key={item.objectID}>
-        <span><a href={item.url}>{item.title}</a></span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        </div>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}><a href={item.url}>{item.title}</a></span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '15%' }}>{item.num_comments}</span>
+        <span style={{ width: '15%' }}>{item.points}</span>
+      </div>
     ) }
   </div>
 
